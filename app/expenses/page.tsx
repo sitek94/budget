809aa740aea09b84584a98f6ec733e3aed02bda4
Maciek Sitkowski <msitkowski94@gmail.com>
2023-01-26 20:06:17 +0100

    add zod

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,4 +1,5 @@
 import * as google from '@googleapis/sheets'
+import {z} from 'zod'
 
 const credentialsFilename = process.env.CREDENTIALS_FILENAME
 const spreadsheetId = process.env.SPREADSHEET_ID
@@ -14,7 +15,14 @@ const sheets = google.sheets({
   auth,
 })
 
-export default async function Expenses() {
+const Transaction = z.object({
+  id: z.string(),
+  description: z.string(),
+  price: z.string(),
+  date: z.string(),
+})
+
+async function fetchTransactionsSheetValues() {
   const spreadsheet = await sheets.spreadsheets.get({
     spreadsheetId,
   })
@@ -26,7 +34,47 @@ export default async function Expenses() {
     range: `${title}!A1:C10`,
   })
 
-  const [header, ...rows] = data.data.values || []
+  const values = data.data.values
+  if (!values) {
+    throw new Error('"Transactions" sheet is empty')
+  }
+
+  return values
+}
+
+const columnOrder = ['description', 'price', 'date'] as const
+const columnIndexMap = columnOrder.reduce(
+  (acc, column, index) => ({...acc, [column]: index}),
+  {} as Record<(typeof columnOrder)[number], number>,
+)
+
+const getTransactionsTable = (
+  data: Awaited<ReturnType<typeof fetchTransactionsSheetValues>>,
+) => {
+  const [columnNames, ...rows] = data
+
+  const transactions = rows.map((row, index) => {
+    const description = row[columnIndexMap.description]
+    const price = row[columnIndexMap.price]
+    const date = row[columnIndexMap.date]
+
+    return Transaction.parse({
+      id: String(index),
+      description,
+      price,
+      date,
+    })
+  })
+
+  return {
+    columnNames,
+    transactions,
+  }
+}
+
+export default async function Expenses() {
+  const sheetValues = await fetchTransactionsSheetValues()
+  const {columnNames, transactions} = getTransactionsTable(sheetValues)
 
   return (
     <>
@@ -34,17 +82,17 @@ export default async function Expenses() {
       <table>
         <thead>
           <tr>
-            {header.map((cell, index) => (
-              <th key={index}>{cell}</th>
+            {columnNames.map(columnName => (
+              <th key={columnName}>{columnName}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {rows.map((row, index) => (
-            <tr key={index}>
-              {row.map((cell, index) => (
-                <td key={index}>{cell}</td>
-              ))}
+          {transactions.map(({id, description, price, date}) => (
+            <tr key={id}>
+              <td>{description}</td>
+              <td>{price}</td>
+              <td>{date}</td>
             </tr>
           ))}
         </tbody>
